293cf53ebb5c0ad2f8e89ef192d761f838cbd93c
Manmatha Mandal <126950623+Manmatha123@users.noreply.github.com>
2023-08-15 08:34:05 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,50 @@
+
+
+ let btns=document.querySelectorAll('.btn');
+let display=document.getElementById('displayid');
+
+
+
+
+
+
+
+let string="";
+
+let array=Array.from(btns);
+
+
+array.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+    
+        if(e.target.innerHTML== '='){   
+            string=eval(string);
+            display.value=string;
+        }
+
+        else if(e.target.innerHTML== 'AC'){   
+            string="";
+            display.value=string;
+        }
+
+
+        else if(e.target.innerHTML== 'DEL'){   
+            string=string.substring(0,string.length-1);
+            display.value=string;
+        }
+
+        else{   
+            string +=e.target.innerHTML;
+            display.value=string;
+        }
+
+    })
+    
+})
+
+
+
+
+
+
+
